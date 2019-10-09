2e48b2854e7deccbeefaa24e26f199cf920f3b20
Andrew <merrillman32@gmail.com>
2019-10-08 20:24:47 -0600

    prettier fix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { MockDB } from "./mockdb";
-import { MariaDB } from "./mariadb";
-import { Request, Response } from "express";
+import { MockDB } from './mockdb';
+import { MariaDB } from './mariadb';
+import { Request, Response } from 'express';
 
-const express = require("express");
-var bodyParser = require("body-parser");
-var cors = require("cors");
-const ip = require("ip");
+const express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+const ip = require('ip');
 const app = express();
 const port = 3000;
 
@@ -16,21 +16,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const db = new MockDB();
 const mariaDB = new MariaDB();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () =>
   console.log(`Example app listening on ${ip.address()}:${port}!`)
 );
 
-app.get("/stories", (req, res) => {
+app.get('/stories', (req, res) => {
   res.send(JSON.stringify(db.getStories()));
 });
 
-app.get("/review-reservations", (req, res) => {
+app.get('/review-reservations', (req, res) => {
   res.send(db.getReviewReservations());
 });
 
-app.get("/reviews", (req, res) => {
+app.get('/reviews', (req, res) => {
   mariaDB
     .getReviews()
     .then(reviews => {
@@ -41,37 +41,37 @@ app.get("/reviews", (req, res) => {
     });
 });
 
-app.post("/reviews", (req, res) => {
+app.post('/reviews', (req, res) => {
   mariaDB
     .addReview(req.body.reviewText, req.body.reviewerID, req.body.storyID)
     .then(res.status(200).send())
     .catch(error => {
       console.log(error.message);
-      const msg = "Review from this user already exists for this story";
+      const msg = 'Review from this user already exists for this story';
       if (error.message == msg) {
         res.status(403).send(msg);
       } else {
-        res.status(500).send("Internal Server Error");
+        res.status(500).send('Internal Server Error');
       }
     });
 });
 
-app.post("/rating", (req, res) => {
+app.post('/rating', (req, res) => {
   mariaDB
     .rateReview(req.body.reviewID, req.body.rating)
     .then(res.status(200).send())
     .catch(error => {
       console.log(error.message);
-      const msg = "Review does not exist";
+      const msg = 'Review does not exist';
       if (error.message == msg) {
         res.status(403).send(msg);
       } else {
-        res.status(500).send("Internal Server Error");
+        res.status(500).send('Internal Server Error');
       }
     });
 });
 
-app.get("/reservations", (req, res) => {
+app.get('/reservations', (req, res) => {
   mariaDB
     .getReservations()
     .then(reservations => {
@@ -82,22 +82,22 @@ app.get("/reservations", (req, res) => {
     });
 });
 
-app.post("/reservations", (req, res) => {
+app.post('/reservations', (req, res) => {
   mariaDB
     .addReservation(req.body.userID, req.body.storyID)
     .then(res.status(200).send())
     .catch(error => {
       console.log(error.message);
-      const msg = "User has already reserved this story for review";
+      const msg = 'User has already reserved this story for review';
       if (error.message == msg) {
         res.status(403).send(msg);
       } else {
-        res.status(500).send("Internal Server Error");
+        res.status(500).send('Internal Server Error');
       }
     });
 });
 
-app.post("/register", (req: Request, res: Response) => {
+app.post('/register', (req: Request, res: Response) => {
   mariaDB
     .register(req.body.UserName, req.body.Password)
     .then(token => {
@@ -105,15 +105,15 @@ app.post("/register", (req: Request, res: Response) => {
     })
     .catch(error => {
       console.log(error.message);
-      if (error.message == "Username is taken") {
-        res.status(401).send("That username is already in use");
+      if (error.message == 'Username is taken') {
+        res.status(401).send('That username is already in use');
       } else {
-        res.status(500).send("Internal Server Error");
+        res.status(500).send('Internal Server Error');
       }
     });
 });
 
-app.post("/login", (req, res) => {
+app.post('/login', (req, res) => {
   mariaDB
     .loginUser(req.body.UserName, req.body.Password)
     .then(token => {
@@ -121,10 +121,10 @@ app.post("/login", (req, res) => {
     })
     .catch(error => {
       console.log(error.message);
-      if (error.message == "Invalid Username or Password") {
-        res.status(401).send("Invalid Username or Password");
+      if (error.message == 'Invalid Username or Password') {
+        res.status(401).send('Invalid Username or Password');
       } else {
-        res.status(500).send("Internal Server Error");
+        res.status(500).send('Internal Server Error');
       }
     });
 });
