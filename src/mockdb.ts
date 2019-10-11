a18e571d26e64ff2fe0ceb1d06d557df67240fc9
Andrew <merrillman32@gmail.com>
2019-10-11 08:36:36 -0600

    styling

--- src/mockdb.ts
+++ src/mockdb.ts
@@ -1,16 +1,36 @@
-import {User} from './types/user';
-import {Story} from './types/story';
-import {ReviewReservation} from './types/review_reservation';
+import { User } from './types/user';
+import { Story } from './types/story';
+import { ReviewReservation } from './types/review_reservation';
 
 export class MockDB {
   private users: User[] = [];
   private stories: Story[] = [];
   private reviewReservations: ReviewReservation[] = [];
 
-  loremText = "lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque non euismod liber pellentesque ac augue lobortis facilisis magna ut molestie odio Ut sollicitudin condimentum venenati praesent ultricies feugiat augue non".split(" ");
-  userNames = ["bettyTheBot", "tommyj", "shj1996", "megera", "bandr", "mstbrn", "hotman", "merric", "neo", "crest", "xkcd", "fanboy", "heyho", "bgibbard", "meyer", "hackerman"];
-  genres = "Mystery Sci-Fi Fantasy Drama Romance Adventure Action Horror Fan-Fiction Humor Other".split(" ");
-
+  loremText = 'lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque non euismod liber pellentesque ac augue lobortis facilisis magna ut molestie odio Ut sollicitudin condimentum venenati praesent ultricies feugiat augue non'.split(
+    ' '
+  );
+  userNames = [
+    'bettyTheBot',
+    'tommyj',
+    'shj1996',
+    'megera',
+    'bandr',
+    'mstbrn',
+    'hotman',
+    'merric',
+    'neo',
+    'crest',
+    'xkcd',
+    'fanboy',
+    'heyho',
+    'bgibbard',
+    'meyer',
+    'hackerman'
+  ];
+  genres = 'Mystery Sci-Fi Fantasy Drama Romance Adventure Action Horror Fan-Fiction Humor Other'.split(
+    ' '
+  );
 
   constructor() {
     this.generateUsers();
@@ -30,134 +50,267 @@ export class MockDB {
     return this.reviewReservations;
   }
 
-
   generateUsers() {
-    const user1 = new User("bettyTheBot", "betty@mailinator.com", [], [], 506);
-    const user2 = new User("tommyj", "tom@mailinator.com", [], [], -12);
-    const user3 = new User("shj1996", "sash@mailinator.com", [], [], 119);
-    const user4 = new User("megera", "booky@mailinator.com", [], [], 95);
-    const user5 = new User("bandr", "band@mailinator.com", [], [], 120);
-    const user6 = new User("mstbrn", "mist@mailinator.com", [], [], 264);
-    const user7 = new User("hotman", "flameo@mailinator.com", [], [], 144);
-    const user8 = new User("merric", "none", [], [], 15);
-    const user9 = new User("neo", "none", [], [], 15);
-    const user10 = new User("crest", "none", [], [], 15);
-    const user11 = new User("xkcd", "none", [], [], 15);
-    const user12 = new User("fanboy", "none", [], [], 15);
-    const user13 = new User("heyho", "none", [], [], 15);
-    const user14 = new User("bgibbard", "none", [], [], 15);
-    const user15 = new User("meyer", "none", [], [], 15);
-    const user16 = new User("hackerman", "none", [], [], 15);
+    const user1 = new User('bettyTheBot', 'betty@mailinator.com', [], [], 506);
+    const user2 = new User('tommyj', 'tom@mailinator.com', [], [], -12);
+    const user3 = new User('shj1996', 'sash@mailinator.com', [], [], 119);
+    const user4 = new User('megera', 'booky@mailinator.com', [], [], 95);
+    const user5 = new User('bandr', 'band@mailinator.com', [], [], 120);
+    const user6 = new User('mstbrn', 'mist@mailinator.com', [], [], 264);
+    const user7 = new User('hotman', 'flameo@mailinator.com', [], [], 144);
+    const user8 = new User('merric', 'none', [], [], 15);
+    const user9 = new User('neo', 'none', [], [], 15);
+    const user10 = new User('crest', 'none', [], [], 15);
+    const user11 = new User('xkcd', 'none', [], [], 15);
+    const user12 = new User('fanboy', 'none', [], [], 15);
+    const user13 = new User('heyho', 'none', [], [], 15);
+    const user14 = new User('bgibbard', 'none', [], [], 15);
+    const user15 = new User('meyer', 'none', [], [], 15);
+    const user16 = new User('hackerman', 'none', [], [], 15);
 
-    this.users = [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11, user12, user13, user14, user15, user16];
-   }
+    this.users = [
+      user1,
+      user2,
+      user3,
+      user4,
+      user5,
+      user6,
+      user7,
+      user8,
+      user9,
+      user10,
+      user11,
+      user12,
+      user13,
+      user14,
+      user15,
+      user16
+    ];
+  }
 
-   generateStories() {
-    const story = new Story("A Wonderous Journey", "a123", "tommyj", "Action", "superfakelink.com", "This story is going to be pretty boring unless you care about Western Canadian history, fair warning :)", 192560, this.generateRandomDate(), 12, []);
-    const story2 = new Story("Modestly Mean", "a124", "shj1996", "Romance", "inkitt.com/also-fake", "This is the first story I've ever finished!! I hope you like it, thanks for reviewing!", 43567, this.generateRandomDate(), 8, []);
-    const story3 = new Story("Uncovered", "uncovered", "bettyTheBot", "Sci-Fi", "http://berkeleyandrus.com/novels/Uncovered.pdf", "A super-hero action story with a bit of mystery mixed in...needs work, but plot has potential", 98567, new Date(2019, 1, 10), 10, []);
-    const story4 = new Story("Those Ills We Bear", "TIWB", "bettyTheBot", "Drama", "inkitt.com/so-fake", "The story of Hamlet, gender bent and put in a modern setting", 49230, new Date(2019, 1, 1), 7, []);
+  generateStories() {
+    const story = new Story(
+      'A Wonderous Journey',
+      'a123',
+      'tommyj',
+      'Action',
+      'superfakelink.com',
+      'This story is going to be pretty boring unless you care about Western Canadian history, fair warning :)',
+      192560,
+      this.generateRandomDate(),
+      12,
+      []
+    );
+    const story2 = new Story(
+      'Modestly Mean',
+      'a124',
+      'shj1996',
+      'Romance',
+      'inkitt.com/also-fake',
+      "This is the first story I've ever finished!! I hope you like it, thanks for reviewing!",
+      43567,
+      this.generateRandomDate(),
+      8,
+      []
+    );
+    const story3 = new Story(
+      'Uncovered',
+      'uncovered',
+      'bettyTheBot',
+      'Sci-Fi',
+      'http://berkeleyandrus.com/novels/Uncovered.pdf',
+      'A super-hero action story with a bit of mystery mixed in...needs work, but plot has potential',
+      98567,
+      new Date(2019, 1, 10),
+      10,
+      []
+    );
+    const story4 = new Story(
+      'Those Ills We Bear',
+      'TIWB',
+      'bettyTheBot',
+      'Drama',
+      'inkitt.com/so-fake',
+      'The story of Hamlet, gender bent and put in a modern setting',
+      49230,
+      new Date(2019, 1, 1),
+      7,
+      []
+    );
     let allStories = [story, story2, story3, story4];
     for (var i = 0; i < 500; i++) {
       this.shuffleLoremText();
       const title = this.generateStoryTitle();
       const id = title + Math.random().toString();
       const blurb = this.generateStoryBlurb();
-      const userName = this.userNames[Math.floor(Math.random()*this.userNames.length)];
-      const genre = this.genres[Math.floor(Math.random()*this.genres.length)];
-      const link = "superfakelink.com";
-      const wordCount = 35000 + Math.floor(Math.random()*100000);
-      const desiredReviews = 2 + Math.floor(Math.random()*8);
+      const userName = this.userNames[
+        Math.floor(Math.random() * this.userNames.length)
+      ];
+      const genre = this.genres[Math.floor(Math.random() * this.genres.length)];
+      const link = 'superfakelink.com';
+      const wordCount = 35000 + Math.floor(Math.random() * 100000);
+      const desiredReviews = 2 + Math.floor(Math.random() * 8);
       const date = this.generateRandomDate();
-      allStories.push(new Story(title, id, userName, genre, link, blurb, wordCount, date, desiredReviews, []));
+      allStories.push(
+        new Story(
+          title,
+          id,
+          userName,
+          genre,
+          link,
+          blurb,
+          wordCount,
+          date,
+          desiredReviews,
+          []
+        )
+      );
     }
     this.stories = allStories;
   }
 
   shuffleLoremText() {
     for (var i = 0; i < 100; i++) {
-        var randomIndex = Math.floor(Math.random()*(this.loremText.length));
-        var randomIndex2 = Math.floor(Math.random()*(this.loremText.length));
-        var temp = this.loremText[randomIndex];
-        this.loremText[randomIndex] = this.loremText[randomIndex2];
-        this.loremText[randomIndex2] = temp;
+      var randomIndex = Math.floor(Math.random() * this.loremText.length);
+      var randomIndex2 = Math.floor(Math.random() * this.loremText.length);
+      var temp = this.loremText[randomIndex];
+      this.loremText[randomIndex] = this.loremText[randomIndex2];
+      this.loremText[randomIndex2] = temp;
     }
   }
 
   generateStoryTitle(): string {
-    let title = "";
-    for (let i = Math.random()*2; i < 3.5; i+=1.0) {
-        let word = this.loremText[Math.floor(2 + i)];
-        word = word[0].toUpperCase() + word.substr(1);
-        title += word + " ";
+    let title = '';
+    for (let i = Math.random() * 2; i < 3.5; i += 1.0) {
+      let word = this.loremText[Math.floor(2 + i)];
+      word = word[0].toUpperCase() + word.substr(1);
+      title += word + ' ';
     }
     if (title.length > 25) {
-        title = title.substr(0, 25);
+      title = title.substr(0, 25);
     }
     return title;
   }
 
   generateStoryBlurb(): string {
-      let blurb = "";
-      for (let i = 5; i < 18; i++) {
-          blurb += this.loremText[i] + " ";
-      }
-      return blurb;
+    let blurb = '';
+    for (let i = 5; i < 18; i++) {
+      blurb += this.loremText[i] + ' ';
+    }
+    return blurb;
   }
 
   generateReviewText(): string {
-      let review = "";
-      for (let i = 0; i < 130; i++) {
-          review += this.loremText[Math.floor(Math.random() * this.loremText.length)] + " ";
-      }
-      return review;
+    let review = '';
+    for (let i = 0; i < 130; i++) {
+      review +=
+        this.loremText[Math.floor(Math.random() * this.loremText.length)] + ' ';
+    }
+    return review;
   }
 
   generateReviews() {
     const stories = this.stories;
-    const goodReview = new ReviewReservation("Uncovered", "shj1996", new Date());
-    const goodReview2 = new ReviewReservation("Those Ills We Bear", "tommyj", this.generateRandomDate(), new Date(), "This was a great story! I really liked your characters. I felt like I could never predict what Jessica would do, but I actually kind of loved that about her character. I thought the plot was pretty good, but the first quarter was a little slow for me. Chapters 4 and 5 felt like they could be combined, for example, and I don't see why chapter 7 is there at all. The biggest weakness though was with setting - I never felt like I had a good idea of where the characters were. Take some time to describe the buildings, the weather, the yellow wallpaper, whatever it takes to give me some hints and let my imagination do the rest.");
-    const goodReview3 = new ReviewReservation("Those Ills We Bear", "shj1996", this.generateRandomDate(), this.generateRandomDate(), "Overall this was pretty solid. I like the way the characters interacted with each other and how you had a few that were constant but most of them fell in and out of the story. It made it seem more realistic. I also liked you pacing, although at some points it felt like you were stretching to hit a specific word count. If you want to improve, I would say my biggest issue was with prose. Tighten it up a little more and try to find your own voice, then keep that voice consistent throughout the story. Also, don't try to use big words if you don't know what they mean.");
-    const goodReview4 = new ReviewReservation("Uncovered", "tommyj", this.generateRandomDate(), new Date(), "This was a great story! I really liked your characters. I felt like I could never predict what Jessica would do, but I actually kind of loved that about her character. I thought the plot was pretty good, but the first quarter was a little slow for me. Chapters 4 and 5 felt like they could be combined, for example, and I don't see why chapter 7 is there at all. The biggest weakness though was with setting - I never felt like I had a good idea of where the characters were. Take some time to describe the buildings, the weather, the yellow wallpaper, whatever it takes to give me some hints and let my imagination do the rest.");
-    const goodReview5 = new ReviewReservation("Uncovered", "shj1996", this.generateRandomDate(), this.generateRandomDate(), "Overall this was pretty solid. I like the way the characters interacted with each other and how you had a few that were constant but most of them fell in and out of the story. It made it seem more realistic. I also liked you pacing, although at some points it felt like you were stretching to hit a specific word count. If you want to improve, I would say my biggest issue was with prose. Tighten it up a little more and try to find your own voice, then keep that voice consistent throughout the story. Also, don't try to use big words if you don't know what they mean.");
-    this.addReviewReservations([goodReview, goodReview2, goodReview3, goodReview4, goodReview5]);
+    const goodReview = new ReviewReservation(
+      'Uncovered',
+      'shj1996',
+      new Date()
+    );
+    const goodReview2 = new ReviewReservation(
+      'Those Ills We Bear',
+      'tommyj',
+      this.generateRandomDate(),
+      new Date(),
+      "This was a great story! I really liked your characters. I felt like I could never predict what Jessica would do, but I actually kind of loved that about her character. I thought the plot was pretty good, but the first quarter was a little slow for me. Chapters 4 and 5 felt like they could be combined, for example, and I don't see why chapter 7 is there at all. The biggest weakness though was with setting - I never felt like I had a good idea of where the characters were. Take some time to describe the buildings, the weather, the yellow wallpaper, whatever it takes to give me some hints and let my imagination do the rest."
+    );
+    const goodReview3 = new ReviewReservation(
+      'Those Ills We Bear',
+      'shj1996',
+      this.generateRandomDate(),
+      this.generateRandomDate(),
+      "Overall this was pretty solid. I like the way the characters interacted with each other and how you had a few that were constant but most of them fell in and out of the story. It made it seem more realistic. I also liked you pacing, although at some points it felt like you were stretching to hit a specific word count. If you want to improve, I would say my biggest issue was with prose. Tighten it up a little more and try to find your own voice, then keep that voice consistent throughout the story. Also, don't try to use big words if you don't know what they mean."
+    );
+    const goodReview4 = new ReviewReservation(
+      'Uncovered',
+      'tommyj',
+      this.generateRandomDate(),
+      new Date(),
+      "This was a great story! I really liked your characters. I felt like I could never predict what Jessica would do, but I actually kind of loved that about her character. I thought the plot was pretty good, but the first quarter was a little slow for me. Chapters 4 and 5 felt like they could be combined, for example, and I don't see why chapter 7 is there at all. The biggest weakness though was with setting - I never felt like I had a good idea of where the characters were. Take some time to describe the buildings, the weather, the yellow wallpaper, whatever it takes to give me some hints and let my imagination do the rest."
+    );
+    const goodReview5 = new ReviewReservation(
+      'Uncovered',
+      'shj1996',
+      this.generateRandomDate(),
+      this.generateRandomDate(),
+      "Overall this was pretty solid. I like the way the characters interacted with each other and how you had a few that were constant but most of them fell in and out of the story. It made it seem more realistic. I also liked you pacing, although at some points it felt like you were stretching to hit a specific word count. If you want to improve, I would say my biggest issue was with prose. Tighten it up a little more and try to find your own voice, then keep that voice consistent throughout the story. Also, don't try to use big words if you don't know what they mean."
+    );
+    this.addReviewReservations([
+      goodReview,
+      goodReview2,
+      goodReview3,
+      goodReview4,
+      goodReview5
+    ]);
     for (var i = 0; i < 100; i++) {
-        const story = stories[Math.floor(Math.random()*stories.length)];
-        const reviewer = this.userNames[Math.floor(Math.random()*this.userNames.length)];
-        if (reviewer == story.author) {
-            i--;
-            continue;
-        }
-        const reservation = new ReviewReservation(story.title, reviewer, this.generateRandomDate());
-        this.users.filter(user => user.getName() == reviewer)[0].addReservedStory(reservation);
-        this.addReviewReservations([reservation]);
+      const story = stories[Math.floor(Math.random() * stories.length)];
+      const reviewer = this.userNames[
+        Math.floor(Math.random() * this.userNames.length)
+      ];
+      if (reviewer == story.author) {
+        i--;
+        continue;
+      }
+      const reservation = new ReviewReservation(
+        story.title,
+        reviewer,
+        this.generateRandomDate()
+      );
+      this.users
+        .filter(user => user.getName() == reviewer)[0]
+        .addReservedStory(reservation);
+      this.addReviewReservations([reservation]);
     }
     for (var i = 0; i < 500; i++) {
-        this.shuffleLoremText();
-        const story = stories[Math.floor(Math.random()*stories.length)];
-        const reviewer = this.userNames[Math.floor(Math.random()*this.userNames.length)];
-        if (reviewer == story.author || story.getReviewsLeft() <= 0) {
-            i--;
-            continue;
-        }
-        const reviewText = this.generateReviewText();
-        const review = new ReviewReservation(story.title, reviewer, this.generateRandomDate(), this.generateRandomDate(), reviewText);
-        this.users.filter(user => user.getName() == reviewer)[0].addReservedStory(review);
-        this.addReviewReservations([review]);
+      this.shuffleLoremText();
+      const story = stories[Math.floor(Math.random() * stories.length)];
+      const reviewer = this.userNames[
+        Math.floor(Math.random() * this.userNames.length)
+      ];
+      if (reviewer == story.author || story.getReviewsLeft() <= 0) {
+        i--;
+        continue;
+      }
+      const reviewText = this.generateReviewText();
+      const review = new ReviewReservation(
+        story.title,
+        reviewer,
+        this.generateRandomDate(),
+        this.generateRandomDate(),
+        reviewText
+      );
+      this.users
+        .filter(user => user.getName() == reviewer)[0]
+        .addReservedStory(review);
+      this.addReviewReservations([review]);
     }
-  } 
+  }
 
   addReviewReservations(revRes: ReviewReservation[]) {
     revRes.forEach(reservation => {
-        let title = reservation.story;
-        this.stories.filter(story => story.title == title)[0].completedReviews.push(reservation);
+      let title = reservation.story;
+      this.stories
+        .filter(story => story.title == title)[0]
+        .completedReviews.push(reservation);
     });
     this.reviewReservations.push(...revRes);
   }
 
   generateRandomDate(): Date {
-      const startDate = new Date(2018, 10, 1);
-      const endDate = new Date(2019, 0, 1);
-      return new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime()));
+    const startDate = new Date(2018, 10, 1);
+    const endDate = new Date(2019, 0, 1);
+    return new Date(
+      startDate.getTime() +
+        Math.random() * (endDate.getTime() - startDate.getTime())
+    );
   }
 }
--- src/types/story.ts
+++ src/types/story.ts
@@ -1,31 +1,42 @@
-import { ReviewReservation } from "./review_reservation";
+import { ReviewReservation } from './review_reservation';
 
 export class Story {
-    title: string;
-    storyID: string;
-    author: string;
-    genre: string;
-    link: string;
-    blurb: string;
-    wordCount: number;
-    datePosted: Date;
-    desiredReviews: number;
-    completedReviews: ReviewReservation[];
+  title: string;
+  storyID: string;
+  author: string;
+  genre: string;
+  link: string;
+  blurb: string;
+  wordCount: number;
+  datePosted: Date;
+  desiredReviews: number;
+  completedReviews: ReviewReservation[];
 
-    constructor(title: string, storyID: string, author: string, genre: string, link: string, blurb: string, wordCount: number, datePosted: Date, desiredReviews: number, completedReviews: ReviewReservation[]) {
-        this.title = title;
-        this.storyID = storyID;
-        this.author = author;
-        this.genre = genre;
-        this.link = link;
-        this.blurb = blurb;
-        this.wordCount = wordCount;
-        this.datePosted = datePosted;
-        this.desiredReviews = desiredReviews;
-        this.completedReviews = completedReviews;
-    }
+  constructor(
+    title: string,
+    storyID: string,
+    author: string,
+    genre: string,
+    link: string,
+    blurb: string,
+    wordCount: number,
+    datePosted: Date,
+    desiredReviews: number,
+    completedReviews: ReviewReservation[]
+  ) {
+    this.title = title;
+    this.storyID = storyID;
+    this.author = author;
+    this.genre = genre;
+    this.link = link;
+    this.blurb = blurb;
+    this.wordCount = wordCount;
+    this.datePosted = datePosted;
+    this.desiredReviews = desiredReviews;
+    this.completedReviews = completedReviews;
+  }
 
-    getReviewsLeft(): number {
-        return this.desiredReviews - this.completedReviews.length;
-    }
+  getReviewsLeft(): number {
+    return this.desiredReviews - this.completedReviews.length;
+  }
 }
