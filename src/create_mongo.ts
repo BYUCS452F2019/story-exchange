4465e8c54117b14090b87c5b598d75645dd791d0
Andrew <merrillman32@gmail.com>
2019-11-16 13:06:19 -0700

    cleaned up

--- src/create_mongo.ts
+++ src/create_mongo.ts
@@ -1,12 +1,14 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/story_exchange';
+var url = 'mongodb://localhost:27017';
+const dbName = 'story_exchange';
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log('Database created!');
-  // db.createCollection("name", function(err, res) {
-  //   if (err) throw err;
-  //   console.log("Collection created!");
-  // });
+  const dbo = db.db(dbName);
+  dbo.createCollection('reservations', function(err, res) {
+    if (err) throw err;
+    console.log('Collection created!');
+  });
   db.close();
 });
--- src/mongodb.ts
+++ src/mongodb.ts
@@ -28,7 +28,6 @@ export class MongoDB {
     return this.client.connect().then((client: MongoClient) => {
       const db: Db = client.db(this.dbName);
       const reservations = db.collection('reservations');
-      // let ret_reserations: Reservation[] = [];
       return reservations
         .find()
         .toArray()
@@ -36,13 +35,6 @@ export class MongoDB {
           return saved_reservations.filter(res => {
             return res.userID == userID;
           });
-          // saved_reservations.forEach(reservation => {
-          //   ret_reserations.push({
-          //     username: user.username,
-          //     password: user.password
-          //   } as UserDto);
-          // });
-          // return ret_reserations;
         });
     });
   }
@@ -67,77 +59,3 @@ export class MongoDB {
       .then(() => null);
   }
 }
-
-// import { Reservation } from './types/reservation';
-// import { MongoClient, Db, Collection } from 'mongodb';
-
-// // const mongo = require('mongodb');
-// // const uuidv4 = require('uuid/v4');
-
-// export class MongoDB {
-//   private client: MongoClient;
-//   private DB: string;
-
-//   constructor() {
-//     const url = 'mongodb://localhost:27017/';
-//     this.client = new MongoClient(url, { useUnifiedTopology: true });
-//     this.DB = 'story_exchange';
-//   }
-
-//   public async getReservationsByUser(userID: number): Promise<Reservation[]> {
-//     return this.client.connect().then((client: MongoClient) => {
-//       const db: Db = client.db(this.DB);
-//       const reservations = db.collection('reservations');
-//       return reservations.find({ userID: userID });
-//     });
-
-//     // return MongoClient.connect({ useUnifiedTopology: true }, function(err, db) {
-//     //   if (err) throw err;
-//     //   const dbo = db.db(DB);
-//     //   return dbo
-//     //     .collection('reservations')
-//     //     .find({})
-//     //     .toArray(function(err, reservations) {
-//     //       if (err) throw err;
-//     //       const reservationsByUser = reservations.filter(
-//     //         (reservation: Reservation) => {
-//     //           return reservation.userID == userID;
-//     //         }
-//     //       );
-//     //       db.close();
-//     //       return reservationsByUser;
-//     //     });
-//     // });
-//   }
-
-//   public async addReservation(userID: number, storyID: number) {
-//     return this.client
-//       .connect()
-//       .then(client => {
-//         const db: Db = client.db(this.DB);
-//         const reservations = db.collection('reservations');
-//         return reservations.insertOne({ userID: userID });
-//       })
-//       .then(() => true);
-
-//     // return this.client.connect().then((client: MongoClient) => {
-//     //   const db: Db = client.db(this.DB);
-//     //   const reservations = db.collection('reservations');
-//     //   reservations.insertOne({ userID: userID });
-//     // });
-
-//     // MongoClient.connect(URL, { useUnifiedTopology: true }, function(err, db) {
-//     //   if (err) throw err;
-//     //   const reservation = { userID: userID, storyID: storyID };
-//     //   const dbo = db.db(DB);
-//     //   dbo.collection('reservations').insertOne(reservation, function(err, res) {
-//     //     if (err) throw err;
-//     //     console.log('inserted');
-//     //     console.log(reservation);
-//     //     console.log(res);
-//     //     db.close();
-//     //   });
-//     // });
-//     // return;
-//   }
-// }
