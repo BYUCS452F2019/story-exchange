560fc1892de14ec043350faa86fa0ded13d89550
Andrew <merrillman32@gmail.com>
2019-11-16 13:13:38 -0700

    fixed db create

--- src/create_mongo.ts
+++ src/create_mongo.ts
@@ -4,8 +4,8 @@ const dbName = 'story_exchange';
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  console.log('Database created!');
   const dbo = db.db(dbName);
+  console.log('Database created!');
   db.createUser({ user: 'admin', pwd: 'secret-password' });
   dbo.createCollection('reservations', function(err, res) {
     if (err) throw err;
